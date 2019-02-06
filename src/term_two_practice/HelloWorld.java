6d62ea20712e63042def20c1cadfcb7a56ce0ba6
Alex Tschopp <altschop@calpoly.edu>
2019-02-05 21:49:28 -0800

    againagian

--- src/term_two_practice/HelloWorld.java
+++ src/term_two_practice/HelloWorld.java
@@ -1,9 +1,5 @@
-package term_two_practice;
+pckage term_two_practice;
 // Term Practice
 public class HelloWorld {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	
-
+Sup dog	
 }
